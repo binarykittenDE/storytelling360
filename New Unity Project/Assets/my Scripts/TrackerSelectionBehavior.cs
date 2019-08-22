cd5d377283e623c0ff721586c2eb1969f4888388
Dima <paralysekid@gmail.com>
2019-08-22 14:44:04 +0200

    Scene Selection + Transition from Image to Ground

---
+++ New Unity Project/Assets/my Scripts/TrackerSelectionBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class TrackerSelectionBehavior : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(name.Equals("Panel Poster"))
+        {
+            SceneManager.LoadScene("ImageTrackerScene");
+        }
+
+        else if (name.Equals("Panel Tisch"))
+        {
+            SceneManager.LoadScene("GroundPlaneScene");
+        }
+    }
+}
