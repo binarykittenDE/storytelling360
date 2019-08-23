d6083b629c7eb94e7b01a69472e6db2238f25665
Dima <paralysekid@gmail.com>
2019-08-23 15:34:07 +0200

    Latest stage with cheap 360 sphere

---
+++ New Unity Project/Assets/my Scripts/SwtchTo360Handler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SwtchTo360Handler : MonoBehaviour
+{
+    private bool isIn360 = false;
+
+    public void SwitchTo360(GameObject movieSphere)
+    {
+        //if (!isIn360)
+        //{
+        //    gameObject.GetComponent<Text>().text = "Zurück";
+        //    movieSphere.SetActive(true);
+        //    isIn360 = true;
+        //} else if (isIn360)
+        //{
+        //    gameObject.GetComponent<Text>().text = "360";
+        //    movieSphere.SetActive(false);
+        //    isIn360 = false;
+        //}
+
+        //GameObject.Find("360Sphere").SetActive(true);
+        //GameObject sphere = GameObject.Find("360Sphere").gameObject;
+        movieSphere.SetActive(true);
+
+        SceneManager.LoadScene("360_sphere_test");
+
+        //GameObject button = GameObject.Find("360 Button");
+        //button.GetComponent<Text>().text = "Zurück";
+    }
+}
